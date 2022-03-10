e6caec1f64a0c03e203d334dd350414020f6535b
guillebonutto <guillebonutto@hotmail.com>
2022-03-10 12:31:21 -0300

    tienda

---
+++ src/main/java/com/mindhub/tiendaderopa/TiendaDeRopaApplication.java
@@ -0,0 +1,13 @@
+package com.mindhub.tiendaderopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaDeRopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaDeRopaApplication.class, args);
+	}
+
+}
