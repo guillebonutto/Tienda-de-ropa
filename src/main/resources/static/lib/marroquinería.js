2a3003c2d8d34b2906e01fe694b20466f017be4a
guillebonutto <guillebonutto@hotmail.com>
2022-03-15 14:28:31 -0300

    filtros

--- src/main/resources/static/lib/marroquinería.js
+++ src/main/resources/static/lib/marroquinería.js
@@ -1,35 +1,48 @@
 var app = new Vue({
-    el:'#app',
-    data: {
-        prendas: [],
-        prendasMarroquinería: [],
+	el: "#app",
+	data: {
+		prendas: [],
+		prendasMarroquinería: [],
+		filterPrecio: [],
+	},
+	created() {
+		this.cargarDatos()
+	},
 
+	methods: {
+		cargarDatos() {
+			axios
+				.get("/api/prendas")
+				.then((response) => {
+					this.prendas = response.data
+					this.cargarPrendasMarroquinería()
+				})
+				.catch((error) => {
+					console.log(error)
+				})
+		},
 
-    },
-    created() {
-        this.cargarDatos();
-       
-    },
-
-    methods: {
-        cargarDatos(){
-            axios
-            .get('/api/prendas')
-            .then((response)=>{
-                this.prendas = response.data
-                this.cargarPrendasMarroquinería();
-
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-            
-        },
-
-        cargarPrendasMarroquinería() {
-            this.prendasMarroquinería = this.prendas.filter(element=> element.tipoArticulo == "MARROQUINERIA")}
-
-
-    },
-
+		cargarPrendasMarroquinería() {
+			this.prendasMarroquinería = this.prendas.filter(
+				(element) => element.tipoArticulo == "MARROQUINERIA"
+			)
+		},
+		filtroCategorias(categoria) {
+			this.prendasMarroquinería = this.prendas.filter((element) =>
+				element.nombrePrenda.contains(categoria)
+			)
+		},
+		filtroPrecio(precioDesde, precioHasta) {
+			this.prendasMarroquinería.forEach((precio) => {
+				if (
+					this.prendasMarroquinería.precio >= precioDesde &&
+					this.prendasMarroquinería.precio <= precioHasta
+				) {
+					this.filterPrecio.push(precio)
+				} else {
+					this.filterPrecio = []
+				}
+			})
+		},
+	},
 })
