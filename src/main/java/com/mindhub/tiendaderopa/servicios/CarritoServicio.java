c392ee3426fc4fe9f79385b66205bb6a8814dfd4
guillebonutto <guillebonutto@hotmail.com>
2022-03-17 01:54:00 -0300

    empezando con servicios

---
+++ src/main/java/com/mindhub/tiendaderopa/servicios/CarritoServicio.java
@@ -0,0 +1,8 @@
+package com.mindhub.tiendaderopa.servicios;
+
+import com.mindhub.tiendaderopa.dtos.CarritoDTO;
+import com.mindhub.tiendaderopa.modelos.Carrito;
+
+public interface CarritoServicio {
+    public Carrito saveCarrito(Carrito carrito);
+}
